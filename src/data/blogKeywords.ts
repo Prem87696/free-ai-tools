1bac16ba0445ec70b48b2e7d114044f79f75956b
PREM <prem92006@gmail.com>
2026-03-17 19:15:32 +0530

    Create blogKeywords.ts

---
+++ src/data/blogKeywords.ts
@@ -0,0 +1,14 @@
+export const blogKeywords = [
+
+"ai tools for students",
+"best free ai tools",
+"ai tools for instagram",
+"ai tools for business",
+"ai content generator free",
+"best ai writing tools",
+"ai tools for youtube",
+"ai tools for freelancers",
+"free ai tools for blogging",
+"top ai tools 2026"
+
+]
