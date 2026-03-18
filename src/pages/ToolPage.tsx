6fc416c0a57afa7b501c5b45f33a94d661904c95
PREM <prem92006@gmail.com>
2026-03-18 20:15:38 +0530

    Update ToolPage.tsx

--- src/pages/ToolPage.tsx
+++ src/pages/ToolPage.tsx
@@ -1,4 +1,4 @@
- import React,{useState,useEffect} from "react"
+import React,{useState,useEffect} from "react"
 import {useParams,Navigate} from "react-router-dom"
 import { getAllTools, ToolConfig } from "../data/tools"
 import {generateContent} from "../services/aiRouter"
