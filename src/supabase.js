011aa6c207efe7978dae55d4521c95b8e0ea20c0
PREM <prem92006@gmail.com>
2026-03-27 22:23:38 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  "https://gkdzomlvfebwynhazcjt.supabase.co",
+  "https://gkdzomlvefbywnhazcjt.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrZHpvbWx2ZWZieXduaGF6Y2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1OTcyMzksImV4cCI6MjA5MDE3MzIzOX0.LVxlx6IIkz6iBrYf0zG7kVOHJMRvN1wl7SZuyFydEw0"
 );
