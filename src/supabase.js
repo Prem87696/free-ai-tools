2f96487727dd0068665f2dcfceb1b07ddcefce72
PREM <prem92006@gmail.com>
2026-03-27 21:11:46 +0530

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  "https://gkdzomlvfebwynhazcjt.supabase.co",
+  "sb_publishable_drpyAPhqUuJy-ib9xcFQDQ_N49Qi9jw"
+);
